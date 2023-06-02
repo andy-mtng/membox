e45df37bc33ce0403401b7a9e70503eb2d3df6ac
Andy N <andynguyen9001@gmail.com>
2023-06-01 19:56:07 -0600

    Set up basic memories page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import LandingPage from "./pages/LandingPage";
+import MemoriesPage from "./pages/MemoriesPage";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
               <Route path="/" element={<LandingPage/>}/>
               <Route path="/login" element={<LoginPage/>}/>
               <Route path="/signup" element={<SignupPage/>}/>
+              <Route path="/memories" element={<MemoriesPage/>}/>
           </Routes>
       </BrowserRouter>
     </div>
