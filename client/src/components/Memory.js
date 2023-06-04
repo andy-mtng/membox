e711694fb94a606052f11138a6e0a168e503b25d
Andy N <andynguyen9001@gmail.com>
2023-06-04 15:40:14 -0600

    Fix date display in memories with moment.

--- client/src/components/Memory.js
+++ client/src/components/Memory.js
@@ -1,14 +1,16 @@
 import { format } from "date-fns";
+import moment from 'moment';
 
 function Memory({ title, description, date, isCoreMemory }) {
-    // const formattedDate = format(date, 'yyyy-MM-dd');
-
+    const momentDate = moment(date);
+    const formattedDate = momentDate.format('YYYY-MM-DD');
+    
     return (
         <div className="flex flex-col border border-black w-96">
             <h1>Title: {title}</h1>
             <p>Description: {description}</p>
             <p>isCoreMemory: {isCoreMemory ? "true" : "false"}</p>
-            {/* <h2>Date: {formattedDate}</h2> */}
+            <h2>Date: {formattedDate}</h2>
         </div>
     )
 }
