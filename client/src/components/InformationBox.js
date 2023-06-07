50e038633c88b4e80ba4528e382d8959d7735726
Andy N <andynguyen9001@gmail.com>
2023-06-06 20:47:43 -0600

    Style information box

--- client/src/components/InformationBox.js
+++ client/src/components/InformationBox.js
@@ -1,24 +1,56 @@
 function InformationBox({ message, type }) {
-    const getClassName = () => {
+    const getClassStyles = () => {
         switch (type) {
           case 'success':
-            return 'success-box';
+            return 'bg-green-200 border-green-600 text-green-800';
           case 'error':
-            return 'error-box';
+            return 'bg-red-200 border-red-600 text-red-800';
           case 'warning':
-            return 'warning-box';
+            return 'bg-yellow-200 border-yellow-600 text-yellow-800';
           case 'info':
-            return 'info-box';
+            return 'bg-blue-200 border-blue-600 text-blue-800';
           default:
             return '';
         }
       };
+
+    const getClassIcon = () => {
+      switch (type) {
+        case 'success':
+          return (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-green-800">
+              <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+            </svg>
+          )
+        case 'error':
+          return (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-red-800">
+              <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
+            </svg>
+          )
+        case 'warning':
+          return (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-yellow-800">
+              <path fillRule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
+            </svg>
+          )
+        case 'info':
+          return (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-blue-800">
+              <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
+            </svg>
+          );
+        default:
+          return '';
+      }
+    }
     
-    const boxClassName = getClassName();
+    const styles = getClassStyles();
+    const icon = getClassIcon();
 
     return (
-        <div className="absolute right-4 top-4 bg-gray-300">
-            <h2>{boxClassName}</h2>
+        <div className={`font-medium flex gap-2 absolute right-4 top-4 z-50 p-2 rounded-md border ${styles}`}>
+            {icon}
             <h1>{message}</h1>
         </div>
     )
