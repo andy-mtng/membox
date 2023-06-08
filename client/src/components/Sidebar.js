c269900abc564de98af514e9e16064b94ab17c00
Andy N <andynguyen9001@gmail.com>
2023-06-07 18:58:24 -0600

    Add sidebar styles

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
 
@@ -6,15 +6,21 @@ function Sidebar() {
     const { user } = useAuthContext();
     const { logout } = useLogout();
 
+    const location = useLocation();
+    const currentPage = location.pathname;
+    console.log("Current page", currentPage);
+
     const handleLogoutClick = (event) => {
         event.preventDefault();
         logout();
     }
 
+    const sideBarItemStyles = "flex gap-2 items-center hover:bg-gray-100 rounded-md"
+
     return (
         <div className="fixed h-full border-r border-gray-300 px-6 flex flex-col justify-between ml-auto">
             <div className="flex flex-col gap-5">
-                <Link to="/memories" className="flex gap-2 items-center relative mt-8 text-base">
+                <Link to="/memories" className="flex gap-2 items-center relative mt-8 text-base mb-6">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-7 h-7">
                         <path d="M3.196 12.87l-.825.483a.75.75 0 000 1.294l7.25 4.25a.75.75 0 00.758 0l7.25-4.25a.75.75 0 000-1.294l-.825-.484-5.666 3.322a2.25 2.25 0 01-2.276 0L3.196 12.87z" />
                         <path d="M3.196 8.87l-.825.483a.75.75 0 000 1.294l7.25 4.25a.75.75 0 00.758 0l7.25-4.25a.75.75 0 000-1.294l-.825-.484-5.666 3.322a2.25 2.25 0 01-2.276 0L3.196 8.87z" />
@@ -22,20 +28,20 @@ function Sidebar() {
                     </svg>
                     <h1 className="font-semibold text-2xl relative bottom-0.5">Membox</h1>
                 </Link>
-                <Link className="flex gap-2 items-center mt-6" to="/memories">
+                <Link className={currentPage === "/memories" ? sideBarItemStyles + " text-black" : sideBarItemStyles + " text-gray-400"} to="/memories">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
                         <path fillRule="evenodd" d="M10 1c3.866 0 7 1.79 7 4s-3.134 4-7 4-7-1.79-7-4 3.134-4 7-4zm5.694 8.13c.464-.264.91-.583 1.306-.952V10c0 2.21-3.134 4-7 4s-7-1.79-7-4V8.178c.396.37.842.688 1.306.953C5.838 10.006 7.854 10.5 10 10.5s4.162-.494 5.694-1.37zM3 13.179V15c0 2.21 3.134 4 7 4s7-1.79 7-4v-1.822c-.396.37-.842.688-1.306.953-1.532.875-3.548 1.369-5.694 1.369s-4.162-.494-5.694-1.37A7.009 7.009 0 013 13.179z" clipRule="evenodd" />
                     </svg>
                     Memories
                 </Link>
-                <Link className="flex gap-2 items-center" to="/profile">
+                <Link className={currentPage === "/profile" ? sideBarItemStyles + " text-black" : sideBarItemStyles + " text-gray-400"} to="/profile">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
                         <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
                     </svg>
                     Profile
                 </Link>
             </div>
-            <Link className="flex gap-2 items-center mb-8" onClick={handleLogoutClick}>
+            <Link className="flex gap-2 items-center mb-8 rounded-md hover:bg-gray-100" onClick={handleLogoutClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                     <path fillRule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z" clipRule="evenodd" />
                     <path fillRule="evenodd" d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z" clipRule="evenodd" />
