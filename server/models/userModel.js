3d3dc1eb6e8c44b9422ad062dc2278bf0058ce64
Andy N <andynguyen9001@gmail.com>
2023-06-10 17:58:35 -0600

    Add blank profile picture to user creation automatically

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -43,7 +43,7 @@ userSchema.statics.signup = async function(email, password) {
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
 
-  const user = await this.create({ email, password: hash })
+  const user = await this.create({ email, password: hash, profileImage: { data: "", contentType: ""} })
 
   return user
 }
