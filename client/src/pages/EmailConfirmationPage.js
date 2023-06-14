5dc16b9dd1d536d893d94a7c7f2bc63d5a1377a0
Andy N <andynguyen9001@gmail.com>
2023-06-13 21:21:19 -0600

    Style email confirmation page

--- client/src/pages/EmailConfirmationPage.js
+++ client/src/pages/EmailConfirmationPage.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 function EmailConfirmationPage() {
     return (
-        <div className="bg-gray-200 w-screen h-screen flex justify-center items-center">
-            <div className="bg-white w-1/2 h-1/3 rounded-sm">
-                <h1>You have succesfully verified your email!</h1>
-                <p>Please <Link to="/login">login</Link> to continue using Membox.</p>
+        <div className="w-screen h-screen flex justify-center items-center">
+            <div className="bg-white w-1/2 h-1/3 rounded-sm shadow-md flex justify-center items-center flex-col">
+                <h1 className="font-bold text-2xl">You have succesfully verified your email!</h1>
+                <p>Please <Link to="/login" className="text-blue-500">login</Link> to continue using Membox.</p>
             </div>
         </div>
     )
